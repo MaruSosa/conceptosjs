5fcf6032c01486eaebbf33364cb7db9b7ac9638f
mariana <marianass712@gmail.com>
2023-10-19 20:01:27 -0300

    se realizo una comparacion de dos numeros

---
+++ js/2-condicionales.js
@@ -0,0 +1,10 @@
+/*if(condicion logica){
+    codigo a ejecutar si se cumple la condicion logica
+}*/
+
+//escriba un programa que pida 3 numeros y escriba por pantalla al mayor de los 3
+let num1= parseInt(prompt('ingrese el primer numero'));
+let num2= parseInt(prompt('ingrese el segundo numero'));
+if(num1>num2){
+    console.log('la variable numero 1 es mayor que la variable numero 2')
+}
