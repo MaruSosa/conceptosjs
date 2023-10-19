006c09f92ebd230df6a7e423f18bcee57f80f940
mariana <marianass712@gmail.com>
2023-10-19 17:41:08 -0300

    nuevos conceptos

--- js/1-variables.js
+++ js/1-variables.js
@@ -1,1 +1,20 @@
-let anioActual= 2023
+let anioActual= 2023;
+const url= "https://universe.rollingcodeschool.com/login";
+/*mostrar mensajes*/
+console.log("este es un mensaje en consola");
+console.log(anioActual);
+console.log(url);
+console.log('Año:'+ (anioActual+2));
+//modificar el contenido de una variable//
+anioActual=2024;
+console.log(anioActual);
+//url='otro valor';//
+console.log(url);
+document.write('<h1>Prueba de titulo</h1>');
+document.write('Año:'+anioActual+ '<br>');
+document.write('Año:'+anioActual);
+//ventanas emergentes//
+alert('esto es una prueba de alerta');
+let color= prompt('ingrese un color');
+console.log(color);
+
