60a27bf5a223c9938a6175643e3aaa27df6e675b
mariana <marianass712@gmail.com>
2023-10-24 18:25:30 -0300

    se practico conceptos de bucles while do while y for

---
+++ js/bucles.js
@@ -0,0 +1,18 @@
+//while//
+// let renglon=1;
+// while(renglon<=10){
+//     document.write(`<p>Renglon número ${renglon}<p>`);
+//     renglon=renglon+1;
+// }
+
+// do while//
+// let renglon=1;
+// do{
+//     document.write(`<p>Renglon número ${renglon}<p>`);
+//     renglon=renglon+1;
+// }while(renglon<=10)
+
+//for//
+for(let contador=5;contador>=1;contador--){
+    document.write(`<p>Cuenta Regresiva ${contador}<p>`);
+}
